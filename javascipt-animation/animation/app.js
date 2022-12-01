8f6ecb439bc10c2facdf94c78691bb136d80a96c
MD. MAHSIN UL ISLAM <mahsin.islam@gmail.com>
2022-12-01 12:55:20 +0600

    working on js animation

---
+++ javascipt-animation/animation/app.js
@@ -0,0 +1,8 @@
+const button = document.querySelector('button');
+const nav = document.querySelector('nav');
+const p = document.querySelector('p');
+
+button.addEventListener('click', ()=>{
+    nav.classList.toggle('nav-slide');
+    p.classList.toggle('fade');
+}) 
